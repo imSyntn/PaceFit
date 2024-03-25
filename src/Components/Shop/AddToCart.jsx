1b55f1c217b5d543f49e2f7437d7cfca265f7a4e
Sayantan Sarkar <imsyntn@gmail.com>
2024-03-25 14:53:35 +0530

    addtocart tick not showing fixed

--- src/Components/Shop/AddToCart.jsx
+++ src/Components/Shop/AddToCart.jsx
@@ -1,26 +1,26 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../Styles/Shop/AddToCart.scss'
 import { FaCartShopping, FaCheck } from "react-icons/fa6";
 import { add } from '../../Redux/slices/CartSlice'
 import { useDispatch, useSelector } from 'react-redux'
 
-const AddToCart = ({id, name, brand, gender, price, imageURL, quantity=1 }) => {
+const AddToCart = ({ id, name, brand, gender, price, imageURL, quantity = 1 }) => {
 
     const cartItems = useSelector(state => state.CartSlice.cartItems)
 
     // console.log(cartItems)
-    const[added, setAdded] = useState(false)
+    const [added, setAdded] = useState(false)
     const dispatch = useDispatch()
 
-    useEffect(()=>{
-        const isAvailable = cartItems.find(item=> item.id == id)
-        if(isAvailable) setAdded(true)
-    },[])
+    useEffect(() => {
+        const isAvailable = cartItems.find(item => item.id == id)
+        if (isAvailable) setAdded(true)
+    }, [cartItems,id])
 
     return (
         <div className="addToCart" onClick={(e) => {
             e.preventDefault()
-            if(!added) {
+            if (!added) {
                 dispatch(add({
                     id,
                     name,
